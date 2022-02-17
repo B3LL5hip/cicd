3ab0512b805532873c47a9b87b4e054f803fadc5
B3ll <bellship99@naver.com>
2022-02-17 16:47:25 +0900

    [DEV] fix linting issue. make my code prettier

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 class Hello:
     def say(self):
         print('hello, world')
+
     def add(self, x, y):
-        #print("x+y=",(x+y))
+        print("x+y=", (x+y))
         a = x
         b = y
         return (a+b)
 
+
 if __name__ == "__main__":
     h = Hello()
     h.say()
-    h.add(2,3)
+    h.add(2, 3)
